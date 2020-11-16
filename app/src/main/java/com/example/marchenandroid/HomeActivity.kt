984b7719576d8870cf57ee13dfc17254cdf82512
George Svitenko <georg.svit@gmail.com>
2020-11-17 00:17:19 +0200

    Navigation for NavBottomBar configured.

--- app/src/main/java/com/example/marchenandroid/HomeActivity.kt
+++ app/src/main/java/com/example/marchenandroid/HomeActivity.kt
@@ -27,7 +27,7 @@ class HomeActivity : AppCompatActivity() {
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications,
-                    R.id.navigation_profile
+                    R.id.navigation_profile, R.id.navigation_children
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
