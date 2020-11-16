b9c87d9539ae91e3c4a7673322d1c82b9e32db2b
George Svitenko <georg.svit@gmail.com>
2020-11-16 22:23:47 +0200

    Password and Email utils added.

---
+++ app/src/main/java/com/example/marchenandroid/util/Util.kt
@@ -0,0 +1,12 @@
+package com.example.marchenandroid.util
+
+import android.util.Patterns
+
+fun isPasswordValid(password: String): Boolean {
+    return password.length > 5
+}
+
+fun isEmailValid(email: String): Boolean {
+    return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+}
+
