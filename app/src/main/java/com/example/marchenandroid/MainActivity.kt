db181edd46fd8e240ba143d48870cda07fe5fbd6
George Svitenko <georg.svit@gmail.com>
2020-11-16 18:24:00 +0200

    Base app structure elements added.

--- app/src/main/java/com/example/marchenandroid/MainActivity.kt
+++ app/src/main/java/com/example/marchenandroid/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.marchenandroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    fun Function(view: View){
+        startActivity(Intent(this, HomeActivity::class.java))
+        finish()
     }
 }
