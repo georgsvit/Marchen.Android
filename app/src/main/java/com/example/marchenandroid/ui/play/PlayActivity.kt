977e11a05b017237ba94d67a1ae0df35d17df015
George Svitenko <georg.svit@gmail.com>
2020-11-17 14:20:52 +0200

    Play activity added.

---
+++ app/src/main/java/com/example/marchenandroid/ui/play/PlayActivity.kt
@@ -0,0 +1,12 @@
+package com.example.marchenandroid.ui.play
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.marchenandroid.R
+
+class PlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_play)
+    }
+}
