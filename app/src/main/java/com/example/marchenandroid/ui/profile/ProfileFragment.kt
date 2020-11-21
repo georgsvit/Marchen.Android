7d714500870132321a0e745e0c8a49761b0e57ef
George Svitenko <georg.svit@gmail.com>
2020-11-22 01:08:05 +0200

    GetIdBtn for user in teacher role added.

--- app/src/main/java/com/example/marchenandroid/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/marchenandroid/ui/profile/ProfileFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -28,6 +29,12 @@ class ProfileFragment : Fragment() {
             startActivity(Intent(context, MainActivity::class.java))
             activity?.finish()
             //findNavController().navigate(AccountFragmentDirections.actionAccountFragmentToLoginFragment())
+        }
+
+        binding.getIdBtn.setOnClickListener {
+            val id = viewModel.getId()
+
+            Toast.makeText(context, "Your Id: $id", Toast.LENGTH_LONG).show()
         }
 
         return binding.root
